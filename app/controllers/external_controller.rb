4d2abc377de76a98452bcbca0eb91f0dcdf0aef6
Gamesrol <gamesrol@tomalared.net>
2012-10-05 21:20:15 +0200

    External y borrar css antiguo.

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -8,11 +8,14 @@ class ExternalController < ApplicationController
 	   if current_user
 		    @po = Post.new
 		 else
-		        redirect_to :controller => 'user_sessions', :action => 'new'
+		        redirect_to  :action => 'login'
 		 end
 	end
 
 	def index
 	end
+	
+	def login
+  end
 
 end
