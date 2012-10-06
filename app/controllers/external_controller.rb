5479e39d856e5628eb294837c8790eff19fc49eb
Gamesrol <gamesrol@tomalared.net>
2012-10-06 23:16:17 +0200

    External( Ya trata los dato. Pero se pierden cuando pasa por el login)

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -6,9 +6,27 @@ class ExternalController < ApplicationController
 
 	def share
 	   if current_user
-		    @po = Post.new
+		    @post = Post.new(params[:post])
+		    
+		    if @post.post_type == 'image'
+        capturanombre = "#{@post.user_id}-#{Time.now}"
+        capturanombre = capturanombre.gsub(" ","")
+        direcion = "public/post/#{capturanombre}.jpg"
+        if params[:img] == nil
+          require 'open-uri'
+          open(direcion, "wb") do |file|
+            file << open(@post.content).read
+          end
+        else
+          File.open(direcion, "wb") do |file|
+            file << open(params[:img]).read
+          end
+        end
+        direcion2 = "/post/#{capturanombre}.jpg"
+        @post.content = direcion2
+      end
 		 else
-		        redirect_to  :action => 'login'
+		    redirect_to  :action => 'login'
 		 end
 	end
 
@@ -16,6 +34,7 @@ class ExternalController < ApplicationController
 	end
 	
 	def login
+	  @post = Post.new(params[:post])
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       redirect_to :action => :share
