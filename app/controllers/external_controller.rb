e689d632396e9b9ae0e903d02df631fc66881ef6
Gamesrol <gamesrol@tomalared.net>
2012-10-07 03:03:30 +0200

    External ( Ya trabaja con todos los datos adecuada mente y se puede logear... Pero si fallas en el segundo login se pierden los datos...)

--- app/controllers/external_controller.rb
+++ app/controllers/external_controller.rb
@@ -5,9 +5,8 @@ class ExternalController < ApplicationController
 	helper :tumble
 
 	def share
+	   @post = Post.new(params[:post])
 	   if current_user
-		    @post = Post.new(params[:post])
-		    
 		    if @post.post_type == 'image'
         capturanombre = "#{@post.user_id}-#{Time.now}"
         capturanombre = capturanombre.gsub(" ","")
@@ -22,11 +21,33 @@ class ExternalController < ApplicationController
             file << open(params[:img]).read
           end
         end
-        direcion2 = "/post/#{capturanombre}.jpg"
+        direcion2 = "http://0.0.0.0:3000/post/#{capturanombre}.jpg"
         @post.content = direcion2
       end
+      
+      if @post.post_type == 'link'
+        require 'metainspector'
+        require 'iconv'
+        @url_link = @post.content
+        doc = MetaInspector.new(@post.content)
+        desc = doc.description
+        @post.title = doc.title
+        if doc.image
+          require 'open-uri'
+          capturanombre = "#{post.user_id}-#{Time.now.to_a.join}"
+          direcion = "public/post/#{capturanombre}.jpg"
+          open(direcion, "wb") do |file|
+            file << open(doc.image).read
+          end
+          img_url = "http://0.0.0.0:3000/post/#{capturanombre}.jpg"
+          @post.content = desc + "\n" + img_url + "\n" + doc.url
+        else
+          @post.content = desc + "\n" + "no-img" + "\n" + doc.url
+        end        
+      end
+      
 		 else
-		    redirect_to  :action => 'login'
+		    redirect_to  :action => 'login', :title => @post.title, :content => @post.content, :post_type => @post.post_type
 		 end
 	end
 
@@ -37,9 +58,7 @@ class ExternalController < ApplicationController
 	  @post = Post.new(params[:post])
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      redirect_to :action => :share
-    else
-      render :action => :login
+      redirect_to :action => :share, 'post[title]' => @post.title, 'post[content]' => @post.content, 'post[post_type]' => @post.post_type
     end
   end
 
