66634d08b97ef03d3b64d1dead39eee8c2f32c21
Gamesrol <gamesrol@tomalared.net>
2012-10-07 04:45:23 +0200

    Share (Ya Funciona correctamente)

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,8 @@
 class Share < ActiveRecord::Base
   attr_accessible :post_id, :user_id
 	validates_uniqueness_of :post_id, :scope => :user_id
+	
+  def self.post_user_share(post, user)
+    find(:first, :conditions => {:post_id => post, :user_id => user})
+  end
 end
