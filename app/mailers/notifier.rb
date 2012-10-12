3e932692667681835c901278979ff628940de6c4
ouruboros <r@terminus.(none)>
2012-10-12 17:54:26 +0200

    Password reset changes

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -23,5 +23,15 @@ class Notifier < ActionMailer::Base
       format.text
     end
   end
+
+  def password_reset_instructions(user)
+    subject      "Password Reset Instructions"
+    from         "noreplay@domain.com"
+    recipients   user.email
+    content_type "text/html"
+    sent_on      Time.now
+    body         :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+  end
+
 end
 
