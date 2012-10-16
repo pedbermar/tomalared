07e6d9f886649664f1dc245bfc467e96898d9465
sarcast <sarcastsv@gmail.com>
2012-10-16 20:08:26 +0200

    diseño para el botón borrar, falta sacar aspecto de la página web http://jqueryui.com/themeroller/

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,6 @@
 class CommentController < ApplicationController
   respond_to :xml, :json
-
-	def delete
+  def delete
     comment = Comment.find(params[:id])
     if comment.destroy
       respond_to do |format|
@@ -10,33 +9,32 @@ class CommentController < ApplicationController
         format.js #added
       end
     end
-	end
-
+  end
 
   def new
     @comment = Comment.create!(:body => params[:body], :post_id => params[:post_id], :user_id => current_user[:id])
 
     # Notificaciones para el propietario del post donde se hace el COMENTARIO
     post = Post.find(params[:post_id])
-		user = User.find(post.user_id)
-		if user.id != @comment.user_id
+    user = User.find(post.user_id)
+    if user.id != @comment.user_id
       note = Notifications.new
-      note.user_id = user.id
-      note.note_type = 1
-      note.from = @comment.user_id
-      note.post_id = @comment.post_id
-      note.save
+    note.user_id = user.id
+    note.note_type = 1
+    note.from = @comment.user_id
+    note.post_id = @comment.post_id
+    note.save
     end
-    
+
     comm = post.comments.uniq_by {|x| x.user_id}
     comm.each do |c|
       if current_user.id != c.user_id
         note = Notifications.new
-        note.user_id = c.user_id
-        note.note_type = 1
-        note.from = @comment.user_id
-        note.post_id = @comment.post_id
-        note.save
+      note.user_id = c.user_id
+      note.note_type = 1
+      note.from = @comment.user_id
+      note.post_id = @comment.post_id
+      note.save
       end
     end
     respond_to do |format|
