67eae92684dccfc3437aa23b3c83dec59ab0a0e3
unknown <rgarciaber@PC-ED246155.ced.junta-andalucia.es>
2012-10-17 13:50:09 +0200

    Formato

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -18,22 +18,22 @@ class CommentController < ApplicationController
     user = User.find(post.user_id)
     if user.id != @comment.user_id
       note = Notifications.new
-    note.user_id = user.id
-    note.note_type = 1
-    note.from = @comment.user_id
-    note.post_id = @comment.post_id
-    note.save
+      note.user_id = user.id
+      note.note_type = 1
+      note.from = @comment.user_id
+      note.post_id = @comment.post_id
+      note.save
     end
 
     comm = post.comments.uniq_by {|x| x.user_id}
     comm.each do |c|
       if current_user.id != c.user_id
         note = Notifications.new
-      note.user_id = c.user_id
-      note.note_type = 1
-      note.from = @comment.user_id
-      note.post_id = @comment.post_id
-      note.save
+        note.user_id = c.user_id
+        note.note_type = 1
+        note.from = @comment.user_id
+        note.post_id = @comment.post_id
+        note.save
       end
     end
     respond_to do |format|
