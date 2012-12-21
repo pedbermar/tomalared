c8f7fb7e5dc0e67a21ad96bba842626eb8d9bb1c
gamesrol <gamesrol@destructor>
2012-12-21 09:47:53 +0100

    Aplication limpio...(y jquery cuadraooo)

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+			$( "#q" ).autocomplete({
+			    source: "/search/searched",
+		        select: function(event, ui) 
+		        {
+	    	      window.location = ("/post/user/" + item.name)
+    	  		}
+    		})
+    		.data("autocomplete")._renderItem = function( ul, item ) 
+    		{
+    			return $( "<li></li>" )
+        		.data( "item.autocomplete", item )
+        		.append( "<a href='/post/"+ item.tipo+ "/"+ item.name+ "'><div class='a-autocomplete'><img width='30' src='"+item.img+"'> "+ item.label + "</div></a>")
+        		.appendTo( ul );
+    		};
+});
