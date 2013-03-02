2b10659c8c0e3edb016591a0245dc15a8f24b977
Gamesrol <gs.gore@gmail.com>
2013-03-02 02:44:25 +0100

    css search

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,13 @@
 module SearchHelper
+  def tag_photo (tag)
+      @tag_foto = Post.find(:all, 
+                              :joins => "JOIN posts_tags pt ON pt.post_id = posts.id",
+                              :conditions => ["pt.tag_id = tags.id AND tags.name = ? AND posts.post_type = ?", tag, "image"],
+                              :order => "rand()",
+                              :limit => 1,
+                              :include => [:tags, :user])
+      if @tag_foto.first                     
+        return @tag_foto.first.content
+      end
+  end
 end
