345b69cbde688b75735f329d02bb7577df1b9c77
Gamesrol <gs.gore@gmail.com>
2013-03-07 00:43:58 +0100

    i18n hace_tanto_tiempo FIX

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,38 +14,39 @@ module ApplicationHelper
             :less_seconds => t('datetime.distance_in_words.less_than_x_seconds.other'), 
             :half_minute => t('datetime.distance_in_words.half_a_minute'), 
             :less_minute => t('datetime.distance_in_words.less_than_x_minutes.one'), 
-            :minutes => t('datetime.prompts.minute'), 
+            :minutes => t('datetime.distance_in_words.about_x_minutes.other'), 
             :one_minute => t('datetime.distance_in_words.x_minutes.one'), 
-            :hours => t('datetime.prompts.hour'), 
+            :hours => t('datetime.distance_in_words.about_x_hours.other'), 
             :one_hour => t('datetime.distance_in_words.about_x_hours.one'), 
-            :days => t('datetime.prompts.day'), 
+            :days => t('datetime.distance_in_words.about_x_days.other'),
             :one_day => t('datetime.distance_in_words.about_x_days.one'),
-            :months => t('datetime.prompts.month'), 
+            :months => t('datetime.distance_in_words.about_x_months.other'), 
             :one_month => t('datetime.distance_in_words.about_x_months.one'), 
-            :years => t('datetime.prompts.year'), 
+            :years => t('datetime.distance_in_words.about_x_years.other'), 
             :one_year => t('datetime.distance_in_words.about_x_years.one'),
            }
   
     case distance_in_minutes
-      when 0..1
-        return (distance_in_minutes==0) ? say[:less_minute] : say[:one_minute] unless include_seconds
-        case distance_in_seconds
-          when 0..4   then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '5')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-          when 5..9   then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '10')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-          when 10..19 then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '20')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-          when 20..39 then "<span class=\"tiempo\">#{say[:half_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-          when 40..59 then "<span class=\"tiempo\">#{say[:less_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-          else             "<span class=\"tiempo\">#{say[:one_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-        end
-      when 2..44           then "<span class=\"tiempo\">#{distance_in_minutes} #{say[:minutes]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-      when 45..89          then "<span class=\"tiempo\">#{say[:one_hour]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-      when 90..1439        then "<span class=\"tiempo\">#{(distance_in_minutes.to_f / 60.0).round} #{say[:hours]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
-      when 1440..2879      then "<span class=\"tiempo\">#{say[:one_day]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-      when 2880..43199     then "<span class=\"tiempo\">#{(distance_in_minutes / 1440).round} #{say[:days]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-      when 43200..86399    then "<span class=\"tiempo\">#{say[:one_month]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-      when 86400..525959   then "<span class=\"tiempo\">#{(distance_in_minutes / 43200).round} #{say[:months]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-      when 525960..1051919 then "<span class=\"tiempo\">#{say[:one_year]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-      else                      "<span class=\"tiempo\">#{(distance_in_minutes / 525960).round} #{say[:years]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
-     end
+    when 0..1
+      return (distance_in_minutes==0) ? say[:less_minute] : say[:one_minute] unless include_seconds
+      case distance_in_seconds
+        when 0..4   then result = "#{say[:less_seconds].sub(/\?/, '5')}"
+        when 5..9   then result = "#{say[:less_seconds].sub(/\?/, '10')}"
+        when 10..19 then result = "#{say[:less_seconds].sub(/\?/, '20')}"
+        when 20..39 then result = "#{say[:half_minute]}"
+        when 40..59 then result = "#{say[:less_minute]}"
+        else             result = "#{say[:one_minute]}"
+      end
+      when 2..44           then result = "#{say[:minutes].sub(/\?/, distance_in_minutes.to_s)}"
+      when 45..89          then result = "#{say[:one_hour]}"
+      when 90..1439        then result = "#{say[:hours].sub(/\?/, (distance_in_minutes.to_f / 60.0).round.to_s)}"
+      when 1440..2879      then result = "#{say[:one_day]}"
+      when 2880..43199     then result = "#{say[:days].sub(/\?/, (distance_in_minutes / 1440).round.to_s)}"
+      when 43200..86399    then result = "#{say[:one_month]}"
+      when 86400..525959   then result = "#{say[:months].sub(/\?/, (distance_in_minutes / 43200).round.to_s)}"
+      when 525960..1051919 then result = "#{say[:one_year]}"
+      else                      result = "#{say[:years].sub(/\?/, (distance_in_minutes / 525960).round.to_s)}"
+    end
+    
   end
 end
