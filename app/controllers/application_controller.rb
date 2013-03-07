61db5f5f601aeb00bf04c45a918ed8dc37957489
gamesrol <gs.gore@gmail.com>
2013-03-07 19:45:05 +0100

    Tiempo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,42 +67,59 @@ class ApplicationController < ActionController::Base
     session[:return_to] = nil
   end
 
-   def hace_tanto_tiempo (from_time, to_time = 0)
+  def hace_tanto_tiempo (from_time, to_time = 0)
     # Traducción de distance_of_time_in_words
     result = ""
 
-    include_seconds = true    
+    include_seconds = true
+    language = 'es'
 
     from_time = from_time.to_time if from_time.respond_to?(:to_time)
     to_time = to_time.to_time if to_time.respond_to?(:to_time)
     distance_in_minutes = (((to_time - from_time).abs)/60).round
     distance_in_seconds = ((to_time - from_time).abs).round
     
-     case distance_in_minutes
+    say = {
+            :less_seconds => t('datetime.distance_in_words.less_than_x_seconds.other'), 
+            :half_minute => t('datetime.distance_in_words.half_a_minute'), 
+            :less_minute => t('datetime.distance_in_words.less_than_x_minutes.one'), 
+            :minutes => t('datetime.prompts.minute'), 
+            :one_minute => t('datetime.distance_in_words.x_minutes.one'), 
+            :hours => t('datetime.prompts.hour'), 
+            :one_hour => t('datetime.distance_in_words.about_x_hours.one'), 
+            :days => t('datetime.prompts.day'), 
+            :one_day => t('datetime.distance_in_words.about_x_days.one'),
+            :months => t('datetime.prompts.month'), 
+            :one_month => t('datetime.distance_in_words.about_x_months.one'), 
+            :years => t('datetime.prompts.year'), 
+            :one_year => t('datetime.distance_in_words.about_x_years.one'),
+           }
+           
+    case distance_in_minutes
     when 0..1
-      return (distance_in_minutes==0) ? t('datetime.distance_in_words.less_than_x_minutes.one') : t('datetime.distance_in_words.about_x_minutes.one') unless include_seconds
+      return (distance_in_minutes==0) ? say[:less_minute] : say[:one_minute] unless include_seconds
       case distance_in_seconds
-        when 0..4   then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '5')}"
-        when 5..9   then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '10')}"
-        when 10..19 then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '20')}"
-        when 20..39 then result = "#{t('datetime.distance_in_words.half_a_minute')}"
-        when 40..59 then result = "#{t('datetime.distance_in_words.less_than_x_minutes.one')}"
-        else             result = "#{t('datetime.distance_in_words.about_x_minutes.one')}"
+      when 0..4   then result = "#{say[:less_seconds].sub(/\?/, '5')}"
+      when 5..9   then result = "#{say[:less_seconds].sub(/\?/, '10')}"
+      when 10..19 then result = "#{say[:less_seconds].sub(/\?/, '20')}"
+      when 20..39 then result = "#{say[:half_minute]}"
+      when 40..59 then result = "#{say[:less_minute]}"
+      else             result = "#{say[:one_minute]}"
       end
-      when 2..44           then result = "#{t('datetime.distance_in_words.about_x_minutes.other').sub(/\?/, distance_in_minutes.to_s)}"
-      when 45..89          then result = "#{t('datetime.distance_in_words.about_x_hours.one')}"
-      when 90..1439        then result = "#{t('datetime.distance_in_words.about_x_hours.other').sub(/\?/, (distance_in_minutes.to_f / 60.0).round.to_s)}"
-      when 1440..2879      then result = "#{t('datetime.distance_in_words.about_x_days.one')}"
-      when 2880..43199     then result = "#{t('datetime.distance_in_words.about_x_days.other').sub(/\?/, (distance_in_minutes / 1440).round.to_s)}"
-      when 43200..86399    then result = "#{t('datetime.distance_in_words.about_x_months.one')}"
-      when 86400..525959   then result = "#{t('datetime.distance_in_words.about_x_months.other').sub(/\?/, (distance_in_minutes / 43200).round.to_s)}"
-      when 525960..1051919 then result = "#{t('datetime.distance_in_words.about_x_years.one')}"
-      else                      result = "#{t('datetime.distance_in_words.about_x_years.other').sub(/\?/, (distance_in_minutes / 525960).round.to_s)}"
+    when 2..44           then result = "#{distance_in_minutes} #{say[:minutes]}"
+    when 45..89          then result = "#{say[:one_hour]}"
+    when 90..1439        then result = "#{(distance_in_minutes.to_f / 60.0).round} #{say[:hours]}"
+    when 1440..2879      then result = "#{say[:one_day]}"
+    when 2880..43199     then result = "#{(distance_in_minutes / 1440).round} #{say[:days]}"
+    when 43200..86399    then result = "#{say[:one_month]}"
+    when 86400..525959   then result = "#{(distance_in_minutes / 43200).round} #{say[:months]}"
+    when 525960..1051919 then result = "#{say[:one_year]}"
+    else                      result = "#{(distance_in_minutes / 525960).round} #{say[:years]}"
     end
-    control = "S"
-    if distance_in_minutes > 1439
-      control = "N"
-    end
+  	control = "S"
+  	if distance_in_minutes > 1439
+  		control = "N"
+  	end
     return "<span class=\"tiempo\">" + result + "<input type=\"hidden\" class=\"controlTiempo\" value=\"" + control + "\" /></span>"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,27 +10,42 @@ module ApplicationHelper
     distance_in_minutes = (((to_time - from_time).abs)/60).round
     distance_in_seconds = ((to_time - from_time).abs).round
   
+    say = {
+            :less_seconds => t('datetime.distance_in_words.less_than_x_seconds.other'), 
+            :half_minute => t('datetime.distance_in_words.half_a_minute'), 
+            :less_minute => t('datetime.distance_in_words.less_than_x_minutes.one'), 
+            :minutes => t('datetime.prompts.minute'), 
+            :one_minute => t('datetime.distance_in_words.x_minutes.one'), 
+            :hours => t('datetime.prompts.hour'), 
+            :one_hour => t('datetime.distance_in_words.about_x_hours.one'), 
+            :days => t('datetime.prompts.day'), 
+            :one_day => t('datetime.distance_in_words.about_x_days.one'),
+            :months => t('datetime.prompts.month'), 
+            :one_month => t('datetime.distance_in_words.about_x_months.one'), 
+            :years => t('datetime.prompts.year'), 
+            :one_year => t('datetime.distance_in_words.about_x_years.one'),
+           }
+  
     case distance_in_minutes
-    when 0..1
-      return (distance_in_minutes==0) ? t('datetime.distance_in_words.less_than_x_minutes.one') : t('datetime.distance_in_words.about_x_minutes.one') unless include_seconds
-      case distance_in_seconds
-        when 0..4   then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '5')}"
-        when 5..9   then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '10')}"
-        when 10..19 then result = "#{t('datetime.distance_in_words.less_than_x_seconds.other').sub(/\?/, '20')}"
-        when 20..39 then result = "#{t('datetime.distance_in_words.half_a_minute')}"
-        when 40..59 then result = "#{t('datetime.distance_in_words.less_than_x_minutes.one')}"
-        else             result = "#{t('datetime.distance_in_words.about_x_minutes.one')}"
-      end
-      when 2..44           then result = "#{t('datetime.distance_in_words.about_x_minutes.other').sub(/\?/, distance_in_minutes.to_s)}"
-      when 45..89          then result = "#{t('datetime.distance_in_words.about_x_hours.one')}"
-      when 90..1439        then result = "#{t('datetime.distance_in_words.about_x_hours.other').sub(/\?/, (distance_in_minutes.to_f / 60.0).round.to_s)}"
-      when 1440..2879      then result = "#{t('datetime.distance_in_words.about_x_days.one')}"
-      when 2880..43199     then result = "#{t('datetime.distance_in_words.about_x_days.other').sub(/\?/, (distance_in_minutes / 1440).round.to_s)}"
-      when 43200..86399    then result = "#{t('datetime.distance_in_words.about_x_months.one')}"
-      when 86400..525959   then result = "#{t('datetime.distance_in_words.about_x_months.other').sub(/\?/, (distance_in_minutes / 43200).round.to_s)}"
-      when 525960..1051919 then result = "#{t('datetime.distance_in_words.about_x_years.one')}"
-      else                      result = "#{t('datetime.distance_in_words.about_x_years.other').sub(/\?/, (distance_in_minutes / 525960).round.to_s)}"
-    end
-    
+      when 0..1
+        return (distance_in_minutes==0) ? say[:less_minute] : say[:one_minute] unless include_seconds
+        case distance_in_seconds
+          when 0..4   then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '5')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+          when 5..9   then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '10')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+          when 10..19 then "<span class=\"tiempo\">#{say[:less_seconds].sub(/\?/, '20')}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+          when 20..39 then "<span class=\"tiempo\">#{say[:half_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+          when 40..59 then "<span class=\"tiempo\">#{say[:less_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+          else             "<span class=\"tiempo\">#{say[:one_minute]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+        end
+      when 2..44           then "<span class=\"tiempo\">#{distance_in_minutes} #{say[:minutes]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+      when 45..89          then "<span class=\"tiempo\">#{say[:one_hour]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+      when 90..1439        then "<span class=\"tiempo\">#{(distance_in_minutes.to_f / 60.0).round} #{say[:hours]}<input type=\"hidden\" class=\"controlTiempo\" value=\"S\" /></span>"
+      when 1440..2879      then "<span class=\"tiempo\">#{say[:one_day]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+      when 2880..43199     then "<span class=\"tiempo\">#{(distance_in_minutes / 1440).round} #{say[:days]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+      when 43200..86399    then "<span class=\"tiempo\">#{say[:one_month]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+      when 86400..525959   then "<span class=\"tiempo\">#{(distance_in_minutes / 43200).round} #{say[:months]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+      when 525960..1051919 then "<span class=\"tiempo\">#{say[:one_year]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+      else                      "<span class=\"tiempo\">#{(distance_in_minutes / 525960).round} #{say[:years]}<input type=\"hidden\" class=\"controlTiempo\" value=\"N\" /></span>"
+     end
   end
 end
