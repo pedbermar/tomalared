dcad6a777fbd6460dd0a46ed6a340245d69f34b2
g <gs.gore@gmail.com>
2013-08-26 02:45:59 +0200

    Fix css Login

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,10 @@ module Fra
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %w(#{config.root}/lib/)
+    
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+       html_tag.html_safe
+    }
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
